eae48faf8f4616bf09a0898ef22c2344d6c40aee
harish1102002 <84316266+harish1102002@users.noreply.github.com>
2023-04-10 11:08:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App(props) {
   const [b,setb]=useState([]);
 
   useEffect(()=>{
-    console.log(p);
+    console.log(b);
     if(b.length==0)
     return;
     const p=b[0].slice();
@@ -42,6 +42,7 @@ function App(props) {
     const p=b[2].slice();
     console.log(p);
     fetch("https://feedback-server-oj2d.onrender.com/5").then((j)=>j.json()).then((j)=>{
+      console.log(j);
     j.forEach((e)=>{for(let i=0;i<b[2].length;i++){p[i][0]+=e.post[i][0];
     p[i][1]+=e.post[i][1];p[i][2]+=e.post[i][2];p[i][3]+=e.post[i][3];}});
       console.log("hi");
